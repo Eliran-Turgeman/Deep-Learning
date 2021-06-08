3909244bfdf61ba41d79587aa26bb0a256be94a6
Eliran Turgeman <elirant@lambda.csf.technion.ac.il>
2021-06-08 21:39:16 +0300

    all experiments results

---
+++ hw2/run_experiment_1.2.sh
@@ -0,0 +1,12 @@
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 32 -L 2 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 64 -L 2 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 128 -L 2 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 256 -L 2 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 32 -L 4 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 64 -L 4 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 128 -L 4 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 256 -L 4 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 32 -L 8 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 64 -L 8 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 128 -L 8 -P 4 -H 100
+srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n test -K 256 -L 8 -P 4 -H 100
